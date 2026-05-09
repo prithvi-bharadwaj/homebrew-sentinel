d3e7e1308524fd5384367d185490354cdb12923c
Prithvi <87623702+prithvi-bharadwaj@users.noreply.github.com>
2026-05-08 17:08:02 -0700

    chore: correct release artifact checksum

--- Casks/sentinel.rb
+++ Casks/sentinel.rb
@@ -1,6 +1,6 @@
 cask "sentinel" do
   version "0.1.0"
-  sha256 "c2379c697c3373c86180788f33ff3b9dc8c8d71461c07163e2d761a3a6"
+  sha256 "c2379c697c3373c86180788f33ff3b9dc8c8d71461c07163e2d706b2d761a3a6"
 
   url "https://github.com/prithvi-bharadwaj/homebrew-sentinel/releases/download/v#{version}/Sentinel-#{version}.dmg"
   name "Sentinel"
